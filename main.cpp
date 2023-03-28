7c0cb300a94b682319194a86696aa9764c67007f
red <80164245+Redgabriel13-red13@users.noreply.github.com>
2023-03-28 20:01:15 -0300

    stuff idk

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include "raylib.h"
 
+const int WIDTH = 1000;
+const int HIGHT = 650;
+
+float barRY = HIGHT / 2;
+float barLY = HIGHT / 2 - 100;
+
+
+
+
 int main() {
-	InitWindow(1000, 650, "RONG (Red's Pong)");
+	InitWindow(WIDTH, HIGHT, "RONG (Red's Pong)");
 	SetWindowState(FLAG_VSYNC_HINT);
 	while (!WindowShouldClose()) {
 
 		BeginDrawing();
 			ClearBackground(BLACK);
 
-			DrawCircle(100, 100, 10, WHITE);
+			DrawCircle(WIDTH/2, barRY, 10, WHITE);
+			DrawRectangle(15, barLY, 15, 200, WHITE);
+			DrawRectangle(WIDTH -30, HIGHT / 2 - 100, 15, 200, WHITE);
 
 			DrawFPS(10,10);
 		EndDrawing();
 
 	}
-	
-
-
 	CloseWindow();
 	return 0;
 }
