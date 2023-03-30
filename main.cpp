17ff2c9f86c46d8da5f253166c2ea7f91f9362b6
red <80164245+Redgabriel13-red13@users.noreply.github.com>
2023-03-30 19:02:34 -0300

    add aceleration to the left pedal

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 const int WIDTH = 1000;
 const int HIGHT = 650;
 
-float barRY = HIGHT / 2;
+float barRY = HIGHT / 2 - 100;
 float barLY = HIGHT / 2 - 100;
+
+float barRYaceleration;
+float barLYaceleration; 
 
 
 
@@ -13,16 +16,29 @@ float barLY = HIGHT / 2 - 100;
 int main() {
 	InitWindow(WIDTH, HIGHT, "RONG (Red's Pong)");
 	SetWindowState(FLAG_VSYNC_HINT);
+//main loop
 	while (!WindowShouldClose()) {
+	//check keys and do stuff with them	
+		if (IsKeyDown(KEY_W)) barRYaceleration -= 2.0f;
+		if (IsKeyDown(KEY_S)) barRYaceleration += 2.0f;
+	
+
+	//make aceleration on the bars
+		if (barRYaceleration > 0) barRYaceleration -= 0.5f;
+		if (barRYaceleration < 0) barRYaceleration += 0.5f;
+		barRY += barRYaceleration;
+
+		//printf("%f \n", barRYaceleration);
+
 
 		BeginDrawing();
 			ClearBackground(BLACK);
-
-			DrawCircle(WIDTH/2, barRY, 10, WHITE);
+			DrawFPS(10, 10);
+			DrawCircle(WIDTH/2, HIGHT/2, 10, WHITE);
 			DrawRectangle(15, barLY, 15, 200, WHITE);
-			DrawRectangle(WIDTH -30, HIGHT / 2 - 100, 15, 200, WHITE);
+			DrawRectangle(WIDTH -30, barRY, 15, 200, WHITE);
 
-			DrawFPS(10,10);
+			
 		EndDrawing();
 
 	}
